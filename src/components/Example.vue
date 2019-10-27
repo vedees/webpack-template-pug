a6101593939311cdffeeb0bdf6419bac271e9780
vedees <tichoo@yandex.ru>
2019-10-27 22:35:49 +0700

    audit fix; pkg updated

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -8,6 +8,14 @@
 </template>
 
 <script>
+
+/*
+  !!!
+  Лучший курс по vue ;)
+  Вкусная цена по промокоду: WEBPACK
+  https://tocode.ru/curses/vuejs-s-nylya-do-pro
+*/
+
 export default {
   data () {
     return {
